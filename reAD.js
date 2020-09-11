6d51da8810c7b769cd81c3a781e7eeaebd31a689
63110 <63110for@list.ru>
2020-09-11 15:40:43 +0300

    upd

--- reAD.js
+++ reAD.js
@@ -1,26 +1,44 @@
 var xlsx = require("xlsx");
 
 var firstBook = xlsx.readFile("test.xlsx");
-//console.log(wkbook.SheetNames)
+console.log(firstBook.SheetNames)
 var listReaderFirst = firstBook.Sheets["Лист1"];
 var dataFirstBook = xlsx.utils.sheet_to_json(listReaderFirst);
-//console.log(wkbook.SheetNames)
+
 var secondBook = xlsx.readFile("test2.xlsx");
+console.log(secondBook.SheetNames)
 var listReaderSec = secondBook.Sheets["Лист1"];
 var dataSecondBook = xlsx.utils.sheet_to_json(listReaderSec);
-// var wkbookSecond = xlsx.readFile("",{cellDates:true});
- console.log(wkbookSecond.SheetNames)
-// var ws2 = wkbookSecond.Sheets['Лист1']
-var Newdata = data.map(function (record) {
-  record.Net = record.KILOVATY - record.ZATRATY;
-  delete record.ADRESS;
+
+var Newdata1 = dataFirstBook.map(function (record) {
+  record.value = record.KILOVATY - record.ZATRATY;
+  record.ADRESS;
   delete record.ZATRATY;
   return record;
 });
-//console.log(Newdata)
 
+var Newdata2=dataSecondBook.map(function (record){
+  delete record.ADRESS;
+  delete record.ZATRATY;
+  record.value;
+  return record;
+});
+
+//console.log(Newdata1)
+//console.log(Newdata2)
+//var newWB;
+
+var merged = new map(function (record){
+  record.Newdata1;
+  record.Newdata2;
+  return record;
+});
+
+console.log(merged);
+/*
 var newWB = xlsx.utils.book_new();
-var newWS = xlsx.utils.json_to_sheet(Newdata);
+var newWS = xlsx.utils.json_to_sheet(Newdata1);
 xlsx.utils.book_append_sheet(newWB, newWS, "Newdata");
 
 xlsx.writeFile(newWB, "New Data File.xlsx");
+*/
