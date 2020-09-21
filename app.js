28501a634c20b43794a1e01b593fa7047c3248de
63110 <63110for@list.ru>
2020-09-21 12:20:42 +0300

    fds

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const multer  = require("multer");
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 const app = express();
  
